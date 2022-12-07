9c63cc070c72f4438bab5bc6ef6f4f31110db0fa
Kaho Cheng <chengy1989@hotmail.com>
2022-12-07 20:47:44 +0900

    WS over, copy pasted

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,7 +1,30 @@
 import "../assets/stylesheets/application.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+//functional react component
+// const Hello = (props) => {
+//   const { greeting, name } = props;
+//   return (
+//     <h1>
+//       {props.greeting} {props.name}
+//     </h1>
+//   );
+// };
+
+//class react component
+// class Hello extends React.Component {
+//   render() {
+//     const { greeting, name } = this.props;
+//     return (
+//       <h1>
+//         {greeting} {name}
+//       </h1>
+//     );
+//   }
+// }
 
 const conatiner = document.querySelector("#root");
 const root = ReactDOM.createRoot(conatiner);
-root.render("hello world");
+root.render(<App />);
