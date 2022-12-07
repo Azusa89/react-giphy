7b511de4e33b4ebe985c2d25fbc41dda39fa55ee
Kaho Cheng <chengy1989@hotmail.com>
2022-12-07 19:39:19 +0900

    project start

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,7 @@
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const conatiner = document.querySelector("#root");
+const root = ReactDOM.createRoot(conatiner);
+root.render("hello world");
